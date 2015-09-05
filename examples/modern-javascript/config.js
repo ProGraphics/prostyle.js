8eedaa954fd1529b09fa59f32878c3fd6f97dfdf
Gary Chamberlain <gary@pro.graphics>
2015-09-05 17:01:21 -0400

    Shadow dancing example

---
+++ examples/modern-javascript/config.js
@@ -0,0 +1,5 @@
+ProStyle.defaultControllers = [
+	{ controllerType: "auto", start: true, stepAdvance: true, restart: true },
+	{ controllerType: "track" },
+	{ controllerType: "resize" }
+];
---
+++ examples/modern-javascript/modern-javascript.js
@@ -0,0 +1,16 @@
+ProStyle.Stories.hello = {
+  item:{
+    text:["MODERN","JAVASCRIPT"],
+    init: {
+      color:"#FFF",
+      align:"center",
+			textShadow:[0,0,0,"rgba(0,0,0,0)"],
+      font:26
+    },
+    charAction: {
+      anim:[1,"sine inout",100,0.47,true,0.1],
+      textShadow:[0,2,4],
+      pos:{z:5}
+    }
+  }
+};
