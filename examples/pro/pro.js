9038f396e733067c07f7291a32a879aac80c4625
Gary Chamberlain <gary@pro.graphics>
2015-09-29 10:06:37 -0400

    v1.2 repeat format

--- examples/pro/pro.js
+++ examples/pro/pro.js
@@ -4,7 +4,7 @@ ProStyle.Stories.logo = {
         setup:{src:"pro-1000x829.png", width:90},
         init:{opacity:3, rot:-10},
         action: {
-            anim:{dur:4, yoyo:true, repeat:1, ease:"sine inout"},
+            anim:{dur:4, repeat:[1,"yoyo"], ease:"sine inout"},
             rotation:10
         }
     }
