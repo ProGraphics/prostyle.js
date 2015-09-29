f760effaba63dc14078c9ea894f83805526f3709
Gary Chamberlain <gary@pro.graphics>
2015-09-29 11:13:25 -0400

    v1.2 repeat format

--- examples/controllers/mousemove/mousemove.js
+++ examples/controllers/mousemove/mousemove.js
@@ -2,14 +2,14 @@ ProStyle.Stories.mousemove = {
     frame: {
         padding:5,
         aspectRatio: 10,
-        init: {background:"#EEE"}
+        init: {bg:"#EEE"}
     },
     items: [
         {   text:"move",
             init: { font: 35, pos:[-45,10], rot:-90 },
             scripts: [
                 { action:{anim:{dur:2.5, ease:"linear"}, rot:810, pos:{x:45}} },
-                { action:{anim:{dur:.25, ease:"sine", rep:9, yoyo:true}, pos:{y:-20}} }
+                { action:{anim:{dur:.25, ease:"sine", rep:[9, 'yoyo']}, pos:{y:-20}} }
             ]
         }
     ]
