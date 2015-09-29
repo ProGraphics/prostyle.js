7c13c36b0cdf696c172b83c6cc82fd34cc56ec4f
Gary Chamberlain <gary@pro.graphics>
2015-09-29 10:54:02 -0400

    stagger

--- examples/item.text/words.js
+++ examples/item.text/words.js
@@ -7,6 +7,6 @@ ProStyle.Stories.words = {
         init:{font:{size:14, lineHeight:100}, width:90},
         lineInit:{ padding:true },
         wordInit:{origin:{y:-50, expand:true}, rot:{x:100}, opacity:0, bg:"orange", border:true, corners:true, padding:true},
-        wordAction:{anim:{dur:6, ease:"elastic", stag:0.12}, rot:false, opacity:false}
+        wordAction:{anim:{dur:6, ease:"elastic", stagger:0.12}, rot:false, opacity:false}
     }
 };
